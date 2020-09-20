345ac8df998c08df5b3351146d9634b734bb3b36
jfcc3101 <30846402+jfcc3101@users.noreply.github.com>
2020-09-20 00:13:00 -0500

    Agregadas algunas escenas específicas
    - Creados diccionarios para los productos y sus variaciones.
    - Agregada la escena para las carnes y un popup que actúa como carrusel

---
+++ Scripts/Comida.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().set_auto_accept_quit(false)
+	pass # Replace with function body.
+
+
+func _notification(what):
+	if (what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST):
+		get_tree().change_scene("res://Escenas/MainScreen.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_BotonVolver_button_up():
+	get_tree().change_scene("res://Escenas/MainScreen.tscn")
+	pass # Replace with function body.
+
+
+func _on_BotonCarnes_button_up():
+	get_tree().change_scene("res://Escenas/Carnes.tscn")
+	pass # Replace with function body.
---
+++ Scripts/MainScreen.gd
@@ -0,0 +1,61 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	cargar_datos()
+	get_tree().set_auto_accept_quit(true)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_BotonComida_button_up():
+	get_tree().change_scene("res://Escenas/Comida.tscn")
+	pass # Replace with function body.
+
+
+func cargar_productos():
+	#print("leyendo records")
+	var file = File.new()
+	if not file.file_exists("res://productos.json"):
+		print("No file saved!")
+		return
+
+	if file.open("res://productos.json", File.READ) != 0:
+		print("Error opening file")
+		return
+
+	Global.dict_productos = parse_json(file.get_line())
+	print(Global.dict_productos)
+	file.close()
+	pass
+	
+
+func cargar_imagenes():
+	var file = File.new()
+	if not file.file_exists("res://imagenes.json"):
+		print("No file saved!")
+		return
+
+	if file.open("res://imagenes.json", File.READ) != 0:
+		print("Error opening file")
+		return
+
+	Global.dict_imagenes = parse_json(file.get_line())
+	print(Global.dict_imagenes)
+	file.close()
+	pass
+
+func cargar_datos():
+	cargar_productos()
+	cargar_imagenes()
+	pass
