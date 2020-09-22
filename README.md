ac992d7a019f879fa42d6cc9e2bf40456b8b0612
jfcc3101 <30846402+jfcc3101@users.noreply.github.com>
2020-09-22 14:56:58 -0500

    Readme modificado

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # catalogo_godot
+
+_Por medio de esta aplicación se busca ayudar a una persona que tiene dificultades de comunicación (afasia) o a niños en proceso de aprendizaje a identificar objetos y productos comunmente usados o consumidos en el hogar._
+
+## Plataforma 
+
+_Este proyecto se desarrolla enfocado en la funcionalidad para dispositivos móviles Android._
+
+## Construido con 🛠
+
+* [Godot Engine] (https://godotengine.org/download/windows) Motor de juego usado en su última versión a la fecha 3.2.3
+
+## Exportación a Android 📱
+
+_Para realizar la correcta exportación de la aplicación se siguen los pasos de la [documentación de Godot Engine](https://docs.godotengine.org/es/stable/getting_started/workflow/export/exporting_for_android.html)_
+
+## Sobre derechos
+
+_Este es un proyecto personal y no tiene ningún fin comercial._
+
+## Captura de pantalla de la aplicación
+
+![pantalla principal](Screenshots/Main.png)
--- Scripts/Carnes.gd
+++ Scripts/Carnes.gd
@@ -51,6 +51,7 @@ func _on_BotonVolver_button_up():
 
 func _on_BotonRes_button_up():
 	img_actual = 0
+	$Muestra/VBoxContainer/HBoxContainer/BotonAnterior.disabled = true
 	producto = "carne"
 	$Muestra.window_title = "Carne de Res"
 	buscar_variantes()
@@ -81,6 +82,7 @@ func _on_BotonAnterior_button_up():
 
 func _on_BotonCerdo_button_up():
 	img_actual = 0
+	$Muestra/VBoxContainer/HBoxContainer/BotonAnterior.disabled = true
 	producto = "cerdo"
 	$Muestra.window_title = "Carne de Cerdo"
 	buscar_variantes()
@@ -93,10 +95,34 @@ func _on_BotonCerdo_button_up():
 
 func _on_BotonPollo_button_up():
 	img_actual = 0
+	$Muestra/VBoxContainer/HBoxContainer/BotonAnterior.disabled = true
 	producto = "pollo"
 	$Muestra.window_title = "Pollo"
 	buscar_variantes()
-	print("Variantes de pollo:", variantes.size())
+	$Muestra/VBoxContainer/Label.text = variantes[img_actual]
+	$Muestra/VBoxContainer/TextureRect.texture = load(imagenes[img_actual])
+	$Muestra.show()
+	pass # Replace with function body.
+
+
+func _on_BotonEmbutidos_button_up():
+	img_actual = 0
+	$Muestra/VBoxContainer/HBoxContainer/BotonAnterior.disabled = true
+	producto = "embutidos"
+	$Muestra.window_title = "Embutidos"
+	buscar_variantes()
+	$Muestra/VBoxContainer/Label.text = variantes[img_actual]
+	$Muestra/VBoxContainer/TextureRect.texture = load(imagenes[img_actual])
+	$Muestra.show()
+	pass # Replace with function body.
+
+
+func _on_BotonPescado_button_up():
+	img_actual = 0
+	$Muestra/VBoxContainer/HBoxContainer/BotonAnterior.disabled = true
+	producto = "pescado"
+	$Muestra.window_title = "Pescados"
+	buscar_variantes()
 	$Muestra/VBoxContainer/Label.text = variantes[img_actual]
 	$Muestra/VBoxContainer/TextureRect.texture = load(imagenes[img_actual])
 	$Muestra.show()
